6642783c47f47830ef58e544c75cf26615521c84
Ramachandran Anantharaman <ramachandran.anantharaman@navis.com>
2020-06-01 13:45:22 +0530

    Cleanup

---
+++ src/main/java/com/sourceopen/gmailclient/WeatherService.java
@@ -0,0 +1,21 @@
+package com.sourceopen.gmailclient;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    public String getWeatherForecast() {
+        RestTemplateBuilder builder = new RestTemplateBuilder();
+        RestTemplate restTemplate = builder.build();
+
+
+        String quote = restTemplate.getForObject(
+                "https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD,tLTCUSD,fUSD", String.class);
+
+        System.out.println("asdf");
+        return quote;
+    }
+}
